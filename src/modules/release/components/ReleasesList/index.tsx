0c2ea1ec7a995d11fe15d0d1a25dfb5168a4c75c
Gleb Colei <gleb.koley@gmail.com>
2026-02-06 20:03:41 +0200

    test

--- src/modules/release/components/ReleasesList/index.tsx
+++ src/modules/release/components/ReleasesList/index.tsx
@@ -10,15 +10,22 @@ import styles from './ReleasesList.module.scss';
 const ReleasesList = async ({ page }: { page: string }) => {
    const currentPage = Number(page);
 
-   if (currentPage === 1) {
-      redirect('/');
-   }
+   // if (currentPage === 1) {
+   //    redirect('/');
+   // }
 
    const { data, error, totalPages } = await getReleasesList({
       period: 'all_time',
-      page: currentPage || 1,
+      page: isNaN(currentPage) ? 1 : currentPage,
    });
-
+   // eslint-disable-next-line no-console
+   console.log('totalPages', totalPages);
+   // eslint-disable-next-line no-console
+   console.log('currentPage', currentPage);
+   // eslint-disable-next-line no-console
+   console.log('data', data);
+   // eslint-disable-next-line no-console
+   console.log('error', error);
    // FIXME: fix totalCountError - https://github.com/supabase/supabase-js/issues/1571
    const hasError = error?.totalCountError;
    const isOutOfBounds = !totalPages || currentPage > totalPages;
