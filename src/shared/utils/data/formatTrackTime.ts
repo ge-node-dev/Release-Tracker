5c5e541bbe24f0471a8cd870741bdcb3d60a28b7
Gleb Colei <gleb.koley@gmail.com>
2026-03-09 19:07:50 +0200

    fix: formatTrackTime value round

--- src/shared/utils/data/formatTrackTime.ts
+++ src/shared/utils/data/formatTrackTime.ts
@@ -1,5 +1,6 @@
 export const formatTrackTime = (seconds: number) => {
-   const m = Math.floor(seconds / 60);
-   const s = Math.floor(seconds % 60);
+   const total = Math.ceil(seconds);
+   const m = Math.floor(total / 60);
+   const s = total % 60;
    return `${m}:${s.toString().padStart(2, '0')}`;
 };
