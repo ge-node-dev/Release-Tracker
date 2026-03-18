8d47584068fcbb5e34b442741f6a0c4b948d56c7
egornikolaew <egor.nikolaev00@icloud.com>
2026-03-18 17:07:37 +0300

    fix this week param page

--- src/app/(main)/(public)/[page]/page.tsx
+++ src/app/(main)/(public)/[page]/page.tsx
@@ -1,23 +1,10 @@
-import { Suspense } from 'react';
+import HomePageBuilder from '@/app/(main)/(public)/HomePageBuilder';
 
-import Loading from '@/app/(main)/(public)/loading';
-import PeriodTabs from '@/modules/release/components/PeriodTabs';
-import ReleaseOfTheWeek from '@/modules/release/components/ReleaseOfTheWeek';
-import ReleasesList from '@/modules/release/components/ReleasesList';
-
-const ThisWeekPageWithPage = async ({ params }: { params: Promise<{ page: string }> }) => {
+const ThisWeekAnyPage = async ({ params }: { params: Promise<{ page: string }> }) => {
    const { page } = await params;
    const currentPage = Number(page) || 1;
 
-   return (
-      <main className="mainContainer">
-         <Suspense fallback={<Loading />}>
-            <ReleaseOfTheWeek />
-            <PeriodTabs currentPeriod={'this_week'} />
-            <ReleasesList page={currentPage} period={'this_week'} />
-         </Suspense>
-      </main>
-   );
+   return <HomePageBuilder page={currentPage} period={'this_week'} />;
 };
 
-export default ThisWeekPageWithPage;
+export default ThisWeekAnyPage;
